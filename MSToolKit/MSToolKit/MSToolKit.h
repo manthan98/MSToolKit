40a0b6c866472c33be63bd175a720c526c1ec862
manthan98 <manthan711@gmail.com>
2019-06-26 19:43:16 -0700

    Add anchors extension

---
+++ MSToolKit/MSToolKit/MSToolKit.h
@@ -0,0 +1,19 @@
+//
+//  MSToolKit.h
+//  MSToolKit
+//
+//  Created by Manthan Shah on 2019-06-26.
+//  Copyright © 2019 Manthan Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MSToolKit.
+FOUNDATION_EXPORT double MSToolKitVersionNumber;
+
+//! Project version string for MSToolKit.
+FOUNDATION_EXPORT const unsigned char MSToolKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSToolKit/PublicHeader.h>
+
+
